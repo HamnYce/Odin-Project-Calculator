b6a126d3abaaa3f6628dabf9569779a430ce00ff
HamnYce <hamadboa@gmail.com>
2021-08-25 15:39:48 +0300

    add basic functions -> calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,30 @@
 function add(a,b) {
-
+    return a+b;
 }
 
 function subtract(a,b) {
-
+    return a-b;
 }
 
 function multiply(a,b) {
-
+    return a*b;
 }
 
 function divide(a,b) {
+    if (b==0) {
+        return "Math Error - Cannot Divide by Zero"
+    }
+    return a/b;
+}
+
+function operate(s) {
 
 }
 
-function sum(a,b) {
-
+function clear() {
+    let display = document.querySelector("#calc-display");
+    display.textContent = "";
 }
 
-function pow(a,b) {
-
-}
+//parse the numbers, check which digit is the seprator between them.
+//use a RegExp to remove the operator with +||*|| etc etc 
