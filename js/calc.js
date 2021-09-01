7191f5548f955d1776389982a5bebb6d9a7f55c5
HamnYce <hamadboa@gmail.com>
2021-09-01 18:46:37 +0300

    optimise rounding add prettier style

--- js/calc.js
+++ js/calc.js
@@ -4,31 +4,37 @@ const allClearButton = document.querySelector('#allClearButton')
 const equalsButton = document.querySelector('#equalsButton')
 const display = document.querySelector("#calc-display");
 const negativeButton = document.querySelector("#negative-button")
-let firstNumber,currentOperator,secondNumber,total,clearTextFlag,reset;
+const decimalButton = document.querySelector("#decimal-button")
+
+let firstNumber,currentOperator,secondNumber,total,clearTextFlag = true,reset;
+
+(function() {
+    buttons.forEach(button => {
+        button.addEventListener('click',textToDisplay)
+    })
+    operators.forEach(operator => {
+        operator.addEventListener('click',operate)
+    })
+    
+    allClearButton.addEventListener('click',allClear)
+    
+    equalsButton.addEventListener('click',equals)
+    
+    negativeButton.addEventListener('click',negativise)
+    
+    decimalButton.addEventListener('click',addDecimalPoint)
+}) ()
 
 
-buttons.forEach(button => {
-    button.addEventListener('click',textToDisplay)
-})
-operators.forEach(operator => {
-    operator.addEventListener('click',operate)
-})
 
-allClearButton.addEventListener('click',allClear)
-
-equalsButton.addEventListener('click',equals)
-
-negativeButton.addEventListener('click',negativise)
-
-
-function add(a,b) {console.log('add');return a+b};
-function subtract(a,b) {console.log('subtract');return a-b};
-function multiply(a,b) {console.log('mult'); return a*b};
-function divide(a,b) {console.log('div');
+function add(a,b) {;return a+b};
+function subtract(a,b) {;return a-b};
+function multiply(a,b) {; return a*b};
+function divide(a,b) {
     if (b == 0) {
         //flag to reset values and avoiding getting NaN
         reset = true;
-        return "~zzZZZzZ - Not Allowed~"
+        return "~zZZZzZ~"
     }
     return a/b
 }
@@ -36,7 +42,12 @@ function divide(a,b) {console.log('div');
 
 function negativise() {
     checkReset()
-    display.textContent = "-" + display.textContent;
+    if(display.textContent.charAt(0) != "-") {
+        display.textContent = "-" + display.textContent;
+    }
+    else {
+        display.textContent = display.textContent.substr(1)
+    }
 }
 
 function operate() {
@@ -46,34 +57,35 @@ function operate() {
     if (currentOperator == "+") {
         secondNumber = Number(display.textContent);
         total = add(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9);
+        display.textContent = roundNumber(total);
         currentOperator = "";
+        checkSize()
     }
     else if (currentOperator == "-") {
         secondNumber = Number(display.textContent);
         total = subtract(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9);
+        display.textContent = roundNumber(total);
         currentOperator = "";
+        checkSize()
     }
     else if (currentOperator == "*") {
         secondNumber = Number(display.textContent);
         total = multiply(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9);
+        display.textContent = roundNumber(total);
         currentOperator = "";
+        checkSize()
     }
     else if (currentOperator == "/") {
         secondNumber = Number(display.textContent);
         total = divide(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9);
+        display.textContent = roundNumber(total);
         currentOperator = "";
+        checkSize()
     }
     //store first number (from display or 'total')
     firstNumber = display.textContent == "" ? 0 : Number(display.textContent);
-    console.log(`first Number: ${firstNumber}`)
     //store operator
     currentOperator = this.textContent
-    console.log(`current OP: ${currentOperator}`)
-    console.log(`second Number: ${secondNumber}`)
     clearTextFlag = true;
 }
 
@@ -82,22 +94,26 @@ function equals() {
     if (currentOperator == "+") {
         secondNumber = Number(display.textContent);
         total = add(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9)
+        display.textContent = roundNumber(total);
+        checkSize()
     }
     else if (currentOperator == "-") {
         secondNumber = Number(display.textContent);
         total = subtract(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9)
+        display.textContent = roundNumber(total);
+        checkSize()
     }
     else if (currentOperator == "*") {
         secondNumber = Number(display.textContent);
         total = multiply(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9)
+        display.textContent = roundNumber(total);
+        checkSize()
     }
     else if (currentOperator == "/") {
         secondNumber = Number(display.textContent);
         total = divide(firstNumber,secondNumber)
-        display.textContent = total.toString().substring(0,9)
+        display.textContent = roundNumber(total);
+        checkSize()
     }
     currentOperator = "";
     clearTextFlag = true;
@@ -109,21 +125,60 @@ function allClear() {
     secondNumber = 0;
     total = 0;
     clearTextFlag = true;
-    currentOperator = ""
+    currentOperator = "";
 }
 
 function textToDisplay() {
+    //division by 0 removal
     checkReset()
+
+    //remove numbers after operator has been presed to start 'fresh'
     if (clearTextFlag) {
         display.textContent = "";
         clearTextFlag = false;
     }
     display.textContent += this.textContent;
+    display.textContent = display.textContent.substring(0,9);
 }
 
 function checkReset() {
     if (reset) {
         allClear()
         reset = false;
+        return true;
+    }
+    return false;
+}
+
+function checkSize() {
+    if (display.textContent.trim().length >= 10 ) {
+        display.textContent = "Size Error";
+        reset = true;
     }
 }
+
+function addDecimalPoint() {
+    if (checkReset()) {
+        clearTextFlag = false;
+    }
+    if (!display.textContent.match(/[.]/g)) {
+        display.textContent += "."
+    }
+}
+
+function roundNumber(n) {
+    if(typeof n == "string") {
+        return n
+    }
+    let s = n.toString()
+    let index = s.indexOf(".")
+
+    let power = 8 - ((index == -1) ? 1 : index);
+    n = n*(Math.pow(10,power))
+    n = Math.round(n)
+    n = n/(Math.pow(10,power))
+
+
+    return n
+}
+
